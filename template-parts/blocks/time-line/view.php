d6ee938dee603c3c362ce74c4980fa71ada1ce7f
Matt Weet <matt@epls.design>
2021-08-09 14:53:54 +0100

    Add preamble and title to Time Line block

--- template-parts/blocks/time-line/view.php
+++ template-parts/blocks/time-line/view.php
@@ -1,14 +1,14 @@
 <?php
+
 /**
- * Flexible layout: Slider
- * Allows the editor to add a slider to the post with slides containing
- * an image, text, optional heading and link.
+ * Flexible layout: Time Line
+ * Redners a time line.
  *
  * @package ezpzconsultations
  */
 
 // Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 // Get Params from get_template_part:
 
@@ -17,34 +17,54 @@ $block_id = $args['block_id'];
 $block_classes = $args['block_classes'];
 $time_lines = $block['time_lines'];
 
+$block_title = $block['title'];
+$block_preamble = $block['preamble'];
+
 $i = 0;
 ?>
 
-<section <?php if($block_id_opt = $block['section_id']) echo 'id="'.strtolower($block_id_opt).'"'; ?> class="<?php echo $block_classes;?>">
+<section <?php if ($block_id_opt = $block['section_id']) echo 'id="' . strtolower($block_id_opt) . '"'; ?> class="<?php echo $block_classes; ?>">
+
+  <?php if ($block_title) : $title_align = $block['title_align']; ?>
+    <header class="row justify-center block-title">
+      <div class="col md-10 lg-8">
+        <h2 class="text-<?php echo $title_align; ?>"><?php echo ezpzconsultations_bracket_tag_replace($block_title); ?></h2>
+      </div>
+    </header>
+  <?php endif; ?>
+
+  <?php if ($block_preamble) : ?>
+    <div class="row justify-center block-preamble">
+      <div class="col md-10 lg-8">
+        <?php echo ezpzconsultations_content($block_preamble); ?>
+      </div>
+    </div>
+  <?php endif; ?>
+
   <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="timeline">
+    <div class="row">
+      <div class="col">
+        <div class="timeline">
           <?php
-            foreach ($time_lines as $time_line):
+          foreach ($time_lines as $time_line) :
 
-              $date = $time_line['date'];
-              $time_title = $time_line['time_title'];
-              $time_text = $time_line['time_text'];
+            $date = $time_line['date'];
+            $time_title = $time_line['time_title'];
+            $time_text = $time_line['time_text'];
 
-              $time_params = array(
-                'block_id' => $block_id,
-                'count' => $i,
-                'time_title' => $time_title,
-                'time_text' => $time_text,
-                'date' => $date,
-              );
-              get_template_part( 'template-parts/components/time-line/time', 'basic', $time_params );
-              $i++;
-            endforeach;
+            $time_params = array(
+              'block_id' => $block_id,
+              'count' => $i,
+              'time_title' => $time_title,
+              'time_text' => $time_text,
+              'date' => $date,
+            );
+            get_template_part('template-parts/components/time-line/time', 'basic', $time_params);
+            $i++;
+          endforeach;
           ?>
-          </div>
         </div>
       </div>
+    </div>
   </div>
 </section>
