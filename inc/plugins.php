2a62fc1217473d6358516313b5e51d640ef5221a
Matt Weet <matt@epls.design>
2021-08-19 10:42:33 +0100

    Force ACF FOnt Awesome plugin activation

--- inc/plugins.php
+++ inc/plugins.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file represents an example of the code that themes would use to register
  * the required plugins.
@@ -18,61 +19,69 @@
  */
 
 
-add_action( 'tgmpa_register', 'ezpzconsultations_register_required_plugins' );
+add_action('tgmpa_register', 'ezpzconsultations_register_required_plugins');
 
 /**
  * Register the required plugins for this theme.
  * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
  */
-function ezpzconsultations_register_required_plugins() {
-	/*
+function ezpzconsultations_register_required_plugins()
+{
+  /*
 	 * Array of plugin arrays. Required keys are name and slug.
 	 * If the source is NOT from the .org repo, then source is also required.
 	 */
-	$plugins = array(
+  $plugins = array(
 
     /**
      * Plugins required by the theme
      */
 
     // Include from an external source
-		array(
-			'name'         => 'ACF Field Group Values',
-			'slug'         => 'acf-field-group-values',
-			'source'       => 'https://github.com/timothyjensen/acf-field-group-values/archive/master.zip',
+    array(
+      'name'         => 'ACF Field Group Values',
+      'slug'         => 'acf-field-group-values',
+      'source'       => 'https://github.com/timothyjensen/acf-field-group-values/archive/master.zip',
       'required'     => true,
       'force_activation'  => true,
       'force_deactivation'  => true,
       'external_url' => 'https://github.com/timothyjensen/acf-field-group-values',
     ),
 
-		array(
-			'name'         => 'Advanced Custom Fields PRO',
-			'slug'         => 'advanced-custom-fields-pro',
-			'source'       => 'https://github.com/wp-premium/advanced-custom-fields-pro/archive/master.zip',
+    array(
+      'name'         => 'Advanced Custom Fields PRO',
+      'slug'         => 'advanced-custom-fields-pro',
+      'source'       => 'https://github.com/wp-premium/advanced-custom-fields-pro/archive/master.zip',
       'required'     => true,
       'force_activation'  => true,
-			'external_url' => 'https://github.com/wp-premium/advanced-custom-fields-pro',
+      'external_url' => 'https://github.com/wp-premium/advanced-custom-fields-pro',
     ),
 
     array(
       //
-			'name'         => 'EZPZ WP Optimise',
-			'slug'         => 'ezpz-optimise',
-			'source'       => 'https://github.com/epls-design/ezpz-cleanup/archive/master.zip',
-      'required'     => true,
-      //'force_activation'  => true, 
-			'external_url' => 'https://github.com/epls-design/ezpz-cleanup',
-    ),
-
-    array(
-      //
-			'name'         => 'EZPZ Cookies',
-			'slug'         => 'ezpz-cookies',
-			'source'       => 'https://github.com/epls-design/ezpz-cookies/archive/master.zip',
+      'name'         => 'EZPZ WP Optimise',
+      'slug'         => 'ezpz-optimise',
+      'source'       => 'https://github.com/epls-design/ezpz-cleanup/archive/master.zip',
       'required'     => true,
       //'force_activation'  => true,
-			'external_url' => 'https://github.com/epls-design/ezpz-cookies',
+      'external_url' => 'https://github.com/epls-design/ezpz-cleanup',
+    ),
+
+    array(
+      //
+      'name'         => 'EZPZ Cookies',
+      'slug'         => 'ezpz-cookies',
+      'source'       => 'https://github.com/epls-design/ezpz-cookies/archive/master.zip',
+      'required'     => true,
+      //'force_activation'  => true,
+      'external_url' => 'https://github.com/epls-design/ezpz-cookies',
+    ),
+
+    array(
+      'name'      => 'Advanced Custom Fields: Font Awesome Field',
+      'slug'      => 'advanced-custom-fields-font-awesome',
+      'required'     => true,
+      'force_activation'  => true,
     ),
 
     /**
@@ -81,62 +90,62 @@ function ezpzconsultations_register_required_plugins() {
      * used most often in web builds.
      */
 
-		// Include from the WordPress Plugin Repository.
-		array(
-			'name'      => 'ACF Content Analysis for Yoast SEO',
-			'slug'      => 'acf-content-analysis-for-yoast-seo',
+    // Include from the WordPress Plugin Repository.
+    array(
+      'name'      => 'ACF Content Analysis for Yoast SEO',
+      'slug'      => 'acf-content-analysis-for-yoast-seo',
     ),
     array(
-			'name'      => 'Autoptimize',
-			'slug'      => 'autoptimize',
+      'name'      => 'Autoptimize',
+      'slug'      => 'autoptimize',
     ),
     array(
-			'name'      => 'User Role Editor',
-			'slug'      => 'user-role-editor',
-		),
-    array(
-			'name'      => 'Wordfence Security',
-			'slug'      => 'wordfence',
+      'name'      => 'User Role Editor',
+      'slug'      => 'user-role-editor',
     ),
     array(
-			'name'      => 'WP Migrate DB',
-			'slug'      => 'wp-migrate-db',
+      'name'      => 'Wordfence Security',
+      'slug'      => 'wordfence',
     ),
     array(
-			'name'      => 'WPS Hide Login',
-			'slug'      => 'wps-hide-login',
+      'name'      => 'WP Migrate DB',
+      'slug'      => 'wp-migrate-db',
     ),
     array(
-			'name'      => 'Yoast SEO',
-			'slug'      => 'wordpress-seo',
-		),
-	);
+      'name'      => 'WPS Hide Login',
+      'slug'      => 'wps-hide-login',
+    ),
+    array(
+      'name'      => 'Yoast SEO',
+      'slug'      => 'wordpress-seo',
+    ),
+  );
 
-	/*
+  /*
 	 * Array of configuration settings.
 	 */
-	$config = array(
-		'id'           => 'ezpzconsultations',
-		'parent_slug'  => 'plugins.php',
-		'capability'   => 'edit_plugins',
-		'dismissable'  => false,
-		'is_automatic' => false,
-		'strings'      => array(
-			'page_title'                      => __( 'Install Required Theme Plugins', 'ezpzconsultations' ),
-			'menu_title'                      => __( 'Theme Plugins', 'ezpzconsultations' ),
-			'notice_can_install_required'     => _n_noop(
-				/* translators: 1: plugin name(s). */
-				'This theme requires the following plugin to operate correctly: %1$s.',
-				'This theme requires the following plugins to operate correctly: %1$s.',
-				'ezpzconsultations'
-			),
-			'notice_can_install_recommended'  => _n_noop(
-				/* translators: 1: plugin name(s). */
-				'This theme recommends the following plugin to add additional functionality: %1$s.',
-				'This theme recommends the following plugins to add additional functionality: %1$s.',
-				'ezpzconsultations'
-			),
-		),
-	);
-	tgmpa( $plugins, $config );
+  $config = array(
+    'id'           => 'ezpzconsultations',
+    'parent_slug'  => 'plugins.php',
+    'capability'   => 'edit_plugins',
+    'dismissable'  => false,
+    'is_automatic' => false,
+    'strings'      => array(
+      'page_title'                      => __('Install Required Theme Plugins', 'ezpzconsultations'),
+      'menu_title'                      => __('Theme Plugins', 'ezpzconsultations'),
+      'notice_can_install_required'     => _n_noop(
+        /* translators: 1: plugin name(s). */
+        'This theme requires the following plugin to operate correctly: %1$s.',
+        'This theme requires the following plugins to operate correctly: %1$s.',
+        'ezpzconsultations'
+      ),
+      'notice_can_install_recommended'  => _n_noop(
+        /* translators: 1: plugin name(s). */
+        'This theme recommends the following plugin to add additional functionality: %1$s.',
+        'This theme recommends the following plugins to add additional functionality: %1$s.',
+        'ezpzconsultations'
+      ),
+    ),
+  );
+  tgmpa($plugins, $config);
 }
